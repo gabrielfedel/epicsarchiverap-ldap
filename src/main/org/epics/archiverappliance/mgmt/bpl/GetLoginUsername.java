2f959a6dea9a1d3c87d646628bc5b9db27201b07
Gustavo Ciotto <gustavociotto@gmail.com>
2017-05-24 12:59:53 +0000

    filter for operations which require authentication

--- src/main/org/epics/archiverappliance/mgmt/bpl/GetLoginUsername.java
+++ src/main/org/epics/archiverappliance/mgmt/bpl/GetLoginUsername.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.epics.archiverappliance.common.BPLAction;
 import org.epics.archiverappliance.config.ConfigService;
 import org.epics.archiverappliance.utils.ui.MimeTypeConstants;
@@ -14,24 +15,28 @@ import org.json.simple.JSONValue;
 
 public class GetLoginUsername implements BPLAction {
 
+	private static final Logger logger = Logger.getLogger(GetLoginUsername.class);
+
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse resp, ConfigService configService)
 			throws IOException {
 
-		String username = req.getRemoteUser();
-		
+		logger.info("Session = " + req.getSession());
+
+		String username = (String) req.getSession().getAttribute("username");
+
 		HashMap<String, Object> infoValues = new HashMap<String, Object>();
 		resp.setContentType(MimeTypeConstants.APPLICATION_JSON);
-		
-		if (username != null) 			
+
+		if (username != null)
 			infoValues.put("username", username);
-		else 
+		else
 			infoValues.put("validation", "No user has been authenticated.");
-		
+
 		try(PrintWriter out = resp.getWriter()) {
 			out.println(JSONValue.toJSONString(infoValues));
 		}
-		
+
 		return;
 	}
 
--- src/main/org/epics/archiverappliance/mgmt/bpl/LoginUser.java
+++ src/main/org/epics/archiverappliance/mgmt/bpl/LoginUser.java
@@ -1,7 +1,10 @@
 package org.epics.archiverappliance.mgmt.bpl;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -9,20 +12,62 @@ import javax.servlet.http.HttpSession;
 import org.apache.log4j.Logger;
 import org.epics.archiverappliance.common.BPLAction;
 import org.epics.archiverappliance.config.ConfigService;
+import org.epics.archiverappliance.utils.ui.MimeTypeConstants;
+import org.json.simple.JSONValue;
 
 public class LoginUser implements BPLAction {
 
 	private static final Logger logger = Logger.getLogger(LoginUser.class);
-	
+
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse resp, ConfigService configService)
 			throws IOException {
-		
-			HttpSession session = req.getSession();
-			
-			logger.info("Username " + req.getAttribute("username") + " logged in");
-			
-			session.setAttribute("username", req.getAttribute("username"));
+
+		HashMap<String, Object> infoValues = new HashMap<String, Object>();
+		resp.setContentType(MimeTypeConstants.APPLICATION_JSON);
+
+		HttpSession session = req.getSession();
+
+		logger.info("Username " + req.getParameter("username") + " trying to log in...");
+
+		logger.info("Session = " + session);
+
+		if (session.getAttribute("username") == null ||
+				session.getAttribute("username") != req.getParameter("username")) {
+
+			try {
+
+				req.login(req.getParameter("username"), req.getParameter("password"));
+
+				logger.info("Login successful!");
+
+				session.setAttribute("username", req.getParameter("username"));
+
+				logger.info("Username " + (String) session.getAttribute("username") + " signed in...");
+
+				infoValues.put("validate", "authenticated");
+			}
+			catch (ServletException  e) {
+
+				logger.info("Login failed with error " + e);
+
+				session.invalidate();
+
+				infoValues.put("error", "failed with error " + e);
+
+				logger.error("Ops", e);
+			}
+
+		}
+		else if (session.getAttribute("username") == req.getParameter("username")) {
+
+			infoValues.put("validate", "user already logged");
+		}
+
+		try(PrintWriter out = resp.getWriter()) {
+			out.println(JSONValue.toJSONString(infoValues));
+		}
+
 	}
 
 }
