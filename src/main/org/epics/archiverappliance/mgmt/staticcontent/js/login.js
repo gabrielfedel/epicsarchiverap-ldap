b951d3631b37010df54b8eb1433335c54c87b109
Gustavo Ciotto <gustavociotto@gmail.com>
2018-03-19 15:41:32 -0300

    adding login  messages and fixing usernames with special characters

--- src/main/org/epics/archiverappliance/mgmt/staticcontent/js/login.js
+++ src/main/org/epics/archiverappliance/mgmt/staticcontent/js/login.js
@@ -3,16 +3,28 @@ function attemptSignUser(e) {
         $.ajax({
                 url: '../bpl/login',
                 dataType: 'json',
-		data: 'username=' + e.data.user.val() + '&password=' + e.data.password.val() ,
+                data: 'username=' + encodeURIComponent(e.data.user.val()) + '&password=' + encodeURIComponent(e.data.password.val()) ,
                 async: false,
                 type: 'post',
                 success: function(data, textStatus, jqXHR) {
-			if (data.validate == "authenticated")
-				window.location = e.data.redirectURL;
-			else if (data.validate == "forbidden" ) 
-				alert("User does not have enough privileges to edit the archiver parameters.");
-			else alert("Failed to sign user: " + data.error)
-                        
+                        if (data.validate == "authenticated")
+                                window.location = e.data.redirectURL;
+                        else {
+                            if (data.validate == "forbidden" )
+                               $("#loginWarning").text("User does not have enough privileges to edit the archiver parameters.");
+
+                            else
+                               $("#loginWarning").text("Invalid credentials");
+
+                            $("#loginWarning").fadeIn ();
+
+                            var timer = setInterval(function () {
+
+                                $("#loginWarning").fadeOut ();
+                                clearInterval (timer);
+
+                            }, 5000);
+                        }
                 },
                 error: function(jqXHR, textStatus, errorThrown) {
                         $("#user").text("Anonymus");
